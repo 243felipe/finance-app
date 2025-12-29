6ec6153cb4ec0c016b1fc4bc87810d957e14f8a7
Felipe Cunha <fcunha326@gmail.com>
2025-12-29 14:00:46 -0400

    commit environment

--- frontend/src/environments/environment.production.ts
+++ frontend/src/environments/environment.production.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  URL_FR_BE: `http://${window.location.hostname}:9000/api`,
-  url: `http://${window.location.hostname}:9000`,
-  urlProduction: `http://${window.location.hostname}:9000/api`,
-  apiUrl: `http://${window.location.hostname}:9000/api`,
+  URL_FR_BE: `https://${window.location.hostname}:9000/api`,
+  url: `https://${window.location.hostname}:9000`,
+  urlProduction: `https://${window.location.hostname}:9000/api`,
+  apiUrl: `https://${window.location.hostname}:9000/api`,
   tokenDev: null
 };
 
